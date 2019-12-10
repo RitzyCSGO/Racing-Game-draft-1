3938c7d751cbe8fa3b449893ba78bcd88b28e74c
RitzyCSGO <rsgriptide@gmail.com>
2019-12-10 16:25:05 +0000

    adding teleport feature when speed = 0

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -6,8 +6,8 @@ namespace UnityStandardAssets.Vehicles.Car
     [RequireComponent(typeof (CarController))]
     public class CarUserControl : MonoBehaviour
     {
+        float timeLeft = 3.0f;
         private CarController m_Car; // the car controller we want to use
-
 
         private void Awake()
         {
@@ -18,15 +18,27 @@ namespace UnityStandardAssets.Vehicles.Car
 
         private void FixedUpdate()
         {
+
             // pass the input to the car!
             float h = Input.GetAxis("Horizontal");
-            float v = Input.GetAxis("Vertical");
-#if !MOBILE_INPUT
+            float v = 1; //Input.GetAxis("Vertical");
             float handbrake = Input.GetAxis("Jump");
             m_Car.Move(h, v, v, handbrake);
-#else
-            m_Car.Move(h, v, v, 0f);
-#endif
+
+            //m_Car.Move(h, v, v, 0f);
+
+            if (m_Car.CurrentSpeed == 0)
+            {
+                //timeLeft is going to be equal to however much time there is left - however much time has gone by.
+                timeLeft = timeLeft - Time.time;
+                if (timeLeft < 0)
+                {
+
+
+                    //gameObject.transform.position.x = mark.transform.position.x
+                }
+            }
         }
+
     }
 }
