5820b69e5fc21906723a9b22149608f7a600023f
RitzyCSGO <rsgriptide@gmail.com>
2019-12-10 10:58:56 +0000

    camera angles

--- Assets/scripts/CarControlActive.cs
+++ Assets/scripts/CarControlActive.cs
@@ -6,9 +6,11 @@ using UnityStandardAssets.Vehicles.Car;
 public class CarControlActive : MonoBehaviour
 {
     public GameObject CarControl;
+    public GameObject DreamCar01;
 
     void Start()
     {
         CarControl.GetComponent <CarController> ().enabled = true;
+        DreamCar01.GetComponent <CarAIControl> ().enabled = true;
     }
 }
